98bca0745e5a6fe2a598373cde6087dae83f9465
ashwith31 <ashwitash@gmail.com>
2021-12-01 12:26:02 +0530

    Added: Get request method and used name as path variable

--- src/main/java/com/bridgelabz/controller/HelloMessagesController.java
+++ src/main/java/com/bridgelabz/controller/HelloMessagesController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,11 @@ public class HelloMessagesController {
 
     @GetMapping("/query")
     public String sayHello2(@RequestParam String name){
+        return "Hello "+name;
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHello3(@PathVariable String name){
         return "Hello "+name;
     }
 }
